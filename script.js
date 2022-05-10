a73ad31d69f5de6cad0078f5124c6c338f62298b
atirrell <austinjtirrell@gmail.com>
2022-05-09 20:53:49 -0500

    added nav functionality for mobile

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const toggleButton = document.getElementsByClassName('toggleButton')[0]
-const navLinks = document.getElementsByClassName('navLinks')[0]
+const toggleButton = document.getElementsByClassName('toggleButton')[0];
+const navLinks = document.getElementsByClassName('navLinks')[0];
 
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active')
-})
+});
+toggleButton.addEventListener('touchstart', () => {
+    navLinks.classList.toggle('active')
+});
